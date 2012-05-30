c0aca317f492886051c192b11ad98c15cf6bb2ea
NCWIT Mobile Application <ncwitmobileapp@gmail.com>
2012-05-30 10:48:18 -0600

    commit

---
+++ NCWITMOBILEAPP-AppEngine/src/com/ncwitmobileapp/server/TechicksmemberServiceLocator.java
@@ -0,0 +1,19 @@
+package com.ncwitmobileapp.server;
+
+import com.google.web.bindery.requestfactory.shared.ServiceLocator;
+
+
+public class TechicksmemberServiceLocator implements ServiceLocator {
+
+	@Override
+	public Object getInstance(Class<?> clazz) {
+		try {
+			return clazz.newInstance();
+		} catch (InstantiationException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
