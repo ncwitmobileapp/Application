ab28844725d8f22a8ea2dfd6a0a68c0837b14c7d
NCWIT Mobile Application <ncwitmobileapp@gmail.com>
2012-09-07 21:54:11 -0600

    commit

---
+++ NCWITMOBILEAPP-Android/.apt_generated/com/ncwitmobileapp/shared/TechicksmemberRequestFactoryDeobfuscatorBuilder.java
@@ -0,0 +1,12 @@
+// Automatically Generated -- DO NOT EDIT
+// com.ncwitmobileapp.shared.TechicksmemberRequestFactory
+package com.ncwitmobileapp.shared;
+import java.util.Arrays;
+import com.google.web.bindery.requestfactory.vm.impl.OperationData;
+import com.google.web.bindery.requestfactory.vm.impl.OperationKey;
+public final class TechicksmemberRequestFactoryDeobfuscatorBuilder extends com.google.web.bindery.requestfactory.vm.impl.Deobfuscator.Builder {
+{
+withRawTypeToken("yofBttdTy9JEjAC72qGT7500u8s=", "com.ncwitmobileapp.shared.TechicksmemberProxy");
+withRawTypeToken("8KVVbwaaAtl6KgQNlOTsLCp9TIU=", "com.google.web.bindery.requestfactory.shared.ValueProxy");
+withRawTypeToken("FXHD5YU0TiUl3uBaepdkYaowx9k=", "com.google.web.bindery.requestfactory.shared.BaseProxy");
+}}
